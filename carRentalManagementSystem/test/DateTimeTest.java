d82f2052d2a5766b9e806eabfa0d4467e210a5a4
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-09-04 14:42:05 +0600

    added test cases for name of the day

--- carRentalManagementSystem/test/DateTimeTest.java
+++ carRentalManagementSystem/test/DateTimeTest.java
@@ -1,4 +1,5 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class DateTimeTest {
 
@@ -8,6 +9,54 @@ class DateTimeTest {
 
     @org.junit.jupiter.api.Test
     void getNameOfDay() {
+        testSat();
+        testSun();
+        testMon();
+        testTue();
+        testWed();
+        testThu();
+        testFri();
+        testInvalidDate();
+    }
+    @Test
+    void testInvalidDate() {
+        DateTime dateTime = new DateTime(-99, 9, 2022);
+        Assertions.assertEquals("", dateTime.getNameOfDay());
+    }
+    @Test
+    void testSat() {
+        DateTime dateTime = new DateTime(3, 9, 2022);
+        Assertions.assertEquals("Saturday", dateTime.getNameOfDay());
+    }
+    @Test
+    void testSun() {
+        DateTime dateTime = new DateTime(4, 9, 2022);
+        Assertions.assertEquals("Sunday", dateTime.getNameOfDay());
+    }
+    @Test
+    void testMon() {
+        DateTime dateTime = new DateTime(5, 9, 2022);
+        Assertions.assertEquals("Monday", dateTime.getNameOfDay());
+    }
+    @Test
+    void testTue() {
+        DateTime dateTime = new DateTime(6, 9, 2022);
+        Assertions.assertEquals("Tuesday", dateTime.getNameOfDay());
+    }
+    @Test
+    void testWed() {
+        DateTime dateTime = new DateTime(7, 9, 2022);
+        Assertions.assertEquals("Wednesday", dateTime.getNameOfDay());
+    }
+    @Test
+    void testThu() {
+        DateTime dateTime = new DateTime(8, 9, 2022);
+        Assertions.assertEquals("Thursday", dateTime.getNameOfDay());
+    }
+    @Test
+    void testFri() {
+        DateTime dateTime = new DateTime(9, 9, 2022);
+        Assertions.assertEquals("Friday", dateTime.getNameOfDay());
     }
 
     @org.junit.jupiter.api.Test
