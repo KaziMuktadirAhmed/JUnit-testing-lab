509b90a23cb31831dafc245400901cdaae3b3930
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-09-04 13:45:17 +0600

    added test class

---
+++ carRentalManagementSystem/test/VehicleTypeTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTypeTest {
+
+    @Test
+    void getSeats() {
+    }
+
+    @Test
+    void getCarSeats() {
+    }
+
+    @Test
+    void setCarSeats() {
+    }
+
+    @Test
+    void getLastMaintenance() {
+    }
+
+    @Test
+    void setLastMaintenance() {
+    }
+
+    @Test
+    void canBeRentedForMinimumDays() {
+    }
+
+    @Test
+    void isUnderMaintenance() {
+    }
+}
